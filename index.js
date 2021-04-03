e6bb2383ea46b070e9aac2a01b8d83428527a9f0
Jon Smith <aardvarkjon94@yahoo.com>
2021-04-02 19:55:11 -0500

    Implimented uuid

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path')
+const { v4: uuid } = require('uuid');
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -13,22 +14,22 @@ app.set('view engine', 'ejs')
 //Fake DB
 const comments = [
     {
-        id: 1,
+        id: uuid(),
         username: "Todd",
         comment: "Lol ThaT IS SO FUNNY"
     },
     {
-        id: 2,
+        id: uuid(),
         username: "Jon",
         comment: "I mean, it was kinda funny"
     },
     {
-        id: 3,
+        id: uuid(),
         username: "Cody",
         comment: "Not funny. Definitely not funny."
     },
     {
-        id: 4,
+        id: uuid(),
         username: "Bert",
         comment: "Me Herbert"
     }
@@ -44,7 +45,7 @@ app.get('/comments/new', (req, res) => {
 //Post route to get data, push it to the fake comments array, then redirect to the index view
 app.post('/comments', (req, res) => {
     const { username, comment } = req.body;
-    comments.push({ username, comment })
+    comments.push({ username, comment, id: uuid() })
     res.redirect('/comments')
 })
 
@@ -52,7 +53,7 @@ app.post('/comments', (req, res) => {
 
 app.get('/comments/:id', (req, res) => {
     const { id } = req.params;
-    const comment = comments.find(c => c.id === parseInt(id));
+    const comment = comments.find(c => c.id === id);
     res.render('comments/show', { comment })
 })
 
